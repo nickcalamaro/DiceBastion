47f48a99ccb42d73c237aec01f903dfd43191a92
nickcalamaro@outlook.com <nickcalamaro@outlook.com>
2026-02-06 00:26:42 +0100

    One day I'll learn what CORS means

--- bgg-bunny/bookings-edge-script.ts
+++ bgg-bunny/bookings-edge-script.ts
@@ -29,7 +29,7 @@ const client = createClient({
 
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type",
 };
 
